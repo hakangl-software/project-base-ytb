9db93df78570b929cc7fafa1abc6b8ebd0fe2164
hakangl-software <hglbilisim@gmail.com>
2026-04-26 22:02:45 +0300

    route oluşturma dinamik route kalsör oluşturma daha olmadı

--- api/app.js
+++ api/app.js
@@ -6,9 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -26,8 +23,14 @@ app.use((req, res, next)=>{
   next();
 })
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index')); // http://localhost:3000
+// app.use('/users', require('./routes/users')); // http://localhost:3000/users
+// app.use('/auditlogs', require('./routes/auditlogs')); // http://localhost:3000/auditlogs
+// app.use('/categories', require('./routes/categories')); // http://localhost:3000/categories
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

12f0fa7f634cef4460b83e106d56ad1dcc066a38
hakangl-software <hglbilisim@gmail.com>
2026-06-03 12:08:56 +0300

    routing altyapısı kuruldu #2

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = true;
+
+/* GET users listing. */
+router.all('*', (req, res, next) => {
+  if(isAuthenticated){
+    next();
+  }else{
+    res.json({success: false, error: "Not authenticated"});
+   };  
+});
 
 
-router.get("/", (req, res, next) => {
-  res.send('respond with a resource');
-})
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({success:true});
+});
+
 
 module.exports = router;
